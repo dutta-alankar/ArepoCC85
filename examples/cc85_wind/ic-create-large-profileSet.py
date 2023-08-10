b9fa0abf7cd945a284621e76eb2833e9d86314fa
dutta-alankar <dutta.alankar@gmail.com>
2023-08-10 17:49:49 +0200

    [FIX] Corrected velocity direction in IC

--- examples/cc85_wind/ic-create-large-profileSet.py
+++ examples/cc85_wind/ic-create-large-profileSet.py
@@ -162,14 +162,10 @@ rr      = np.sqrt((data['pos'][:,0] - SpherePos[0])**2 + \
 data['mass'][:] = rho0*rhoTld(rr/(Rinj/pc)) / UnitDensity_in_g_per_cm3
 data['u'][:]    = (1/(gamma-1))* ((P0*prsTld(rr/(Rinj/pc)))/(rho0*rhoTld(rr/(Rinj/pc)))) /  UnitU_in_cm_per_s_sq
 vel = (v0*velTld(rr/(Rinj/pc))) / UnitVelocity_in_cm_per_s
-sintheta = np.sqrt(data['pos'][:,0]**2 + data['pos'][:,1]**2)/np.sqrt(data['pos'][:,0]**2 + data['pos'][:,1]**2 + data['pos'][:,2]**2)
-costheta = data['pos'][:,2]/np.sqrt(data['pos'][:,0]**2 + data['pos'][:,1]**2 + data['pos'][:,2]**2)
-cosphi = data['pos'][:,0]/np.sqrt(data['pos'][:,0]**2 + data['pos'][:,1]**2)
-sinphi = data['pos'][:,1]/np.sqrt(data['pos'][:,0]**2 + data['pos'][:,1]**2)
 
-data['vel'][:,0]  = vel * sintheta * cosphi
-data['vel'][:,1]  = vel * sintheta * sinphi
-data['vel'][:,2]  = vel * costheta
+data['vel'][:,0]  = vel * (data['pos'][:,0] - SpherePos[0])/rr
+data['vel'][:,1]  = vel * (data['pos'][:,1] - SpherePos[1])/rr
+data['vel'][:,2]  = vel * (data['pos'][:,2] - SpherePos[2])/rr
 
 # Remove normal AREPO cells from volume bounded by boundary sphere
 flag    = data['flga']
